b3aee8e7428b1416196c517d18d5cf851baab2ba
Dmitry Yadrikhinsky <dyadrihinsky@i-neti.ru>
2021-04-10 12:39:50 +0300

    [TaskControl] style

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -3,6 +3,8 @@ import { Route, Switch, Link } from 'react-router-dom';
 import { Layout } from 'antd';
 import { observer } from 'mobx-react';
 
+import './Main.less';
+
 import Projects from './projects/Projects';
 import TaskControl from './projects/components/TaskControl/TaskControl';
 import HeaderMenu from '../components/HeaderMenu';
@@ -13,15 +15,14 @@ export default observer(function Main() {
   return (
     <Layout className="layout">
       <Header>
-        <div>
-          <HeaderMenu>
-            <Link to="/hours">Hours</Link>
-          </HeaderMenu>
-          <HeaderMenu>
-            <Link to="/projects">Projects</Link>
-          </HeaderMenu>
-          <TaskControl />
-        </div>
+        <HeaderMenu>
+          <Link to="/hours">Hours</Link>
+        </HeaderMenu>
+        <HeaderMenu>
+          <Link to="/projects">Projects</Link>
+        </HeaderMenu>
+        <span className="flex-1" />
+        <TaskControl />
       </Header>
       <Switch>
         <Route path="/hours">
