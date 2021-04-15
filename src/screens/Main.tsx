f9ee904313347872d46ef1682fe50776e880fe85
Dmitry Yadrikhinsky <dyadrihinsky@i-neti.ru>
2021-04-15 20:52:31 +0300

    Redirect to projects

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
+import { Route, Switch, Link, Redirect } from 'react-router-dom';
 import { Layout } from 'antd';
 import { observer } from 'mobx-react';
 
@@ -26,6 +26,9 @@ export default observer(function Main() {
         <TaskControl />
       </Header>
       <Switch>
+        <Route exact path="/">
+          <Redirect to="/projects" />
+        </Route>
         <Route path="/hours">
           <HoursView />
         </Route>
