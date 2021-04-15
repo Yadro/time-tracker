874787acd0311f37deacdcef321ebbbd226192da
Dmitry Yadrikhinsky <dyadrihinsky@i-neti.ru>
2021-04-15 11:59:16 +0300

    Format date

--- src/screens/hours/components/SelectDate/SelectDate.tsx
+++ src/screens/hours/components/SelectDate/SelectDate.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, DatePicker } from 'antd';
+import { Button, DatePicker, Space } from 'antd';
 import { LeftOutlined, RightOutlined } from '@ant-design/icons';
 import moment from 'moment';
 import addDays from 'date-fns/addDays';
@@ -15,7 +15,7 @@ function changeDate(date: Date, offsetDay: number) {
 
 export default function SelectDate({ date, onChange }: SelectDateProps) {
   return (
-    <div>
+    <Space direction="horizontal">
       <Button
         icon={<LeftOutlined />}
         onClick={() => onChange(changeDate(date, -1))}
@@ -25,11 +25,13 @@ export default function SelectDate({ date, onChange }: SelectDateProps) {
         onChange={(date) => onChange(date?.toDate() || new Date())}
         allowClear={false}
         placeholder="Date"
+        inputReadOnly
+        format={'ddd DD MMM'}
       />
       <Button
         icon={<RightOutlined />}
         onClick={() => onChange(changeDate(date, 1))}
       />
-    </div>
+    </Space>
   );
 }
