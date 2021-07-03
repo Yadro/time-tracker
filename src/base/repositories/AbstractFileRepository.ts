fc42ed250d9ea8279ce9ab485b3d0eebd4b5dace
yadro <yadrone@gmail.com>
2021-07-03 14:30:45 +0300

    Async saving files

--- src/base/repositories/AbstractFileRepository.ts
+++ src/base/repositories/AbstractFileRepository.ts
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+import FsHelper from '../../helpers/FsHelper';
+
 const APP_FOLDER = 'YadroTimeTracker';
 const PROFILE_FOLDER = 'profile1';
 
@@ -16,7 +18,7 @@ export default abstract class AbstractFileRepository<T = any> {
     return path.join(
       AbstractFileRepository.appDataFolder,
       APP_FOLDER,
-      PROFILE_FOLDER
+      PROFILE_FOLDER,
     );
   }
 
@@ -27,22 +29,14 @@ export default abstract class AbstractFileRepository<T = any> {
   public restore(defaultValue: T): T {
     if (fs.existsSync(this.filePath)) {
       const data = fs.readFileSync(this.filePath);
+      // TODO handle parse error. Backup file with issues and return defaultValue
       return JSON.parse(data);
     }
     return defaultValue;
   }
 
   public save(data: T) {
-    [
-      path.join(AbstractFileRepository.appDataFolder, APP_FOLDER),
-      AbstractFileRepository.profileFolder,
-    ].forEach((p) => AbstractFileRepository.createFolderIfNotExists(p));
-    fs.writeFileSync(this.filePath, JSON.stringify(data), 'utf-8');
-  }
-
-  private static createFolderIfNotExists(path: string) {
-    if (!fs.existsSync(path)) {
-      fs.mkdirSync(path);
-    }
+    FsHelper.mkdirIfNotExists(AbstractFileRepository.profileFolder);
+    return FsHelper.writeFile(this.filePath, data);
   }
 }
