3ad729564bdeb611a5f00005487d78f72d5500f1
Dmitry Yadrikhinsky <yadrone@gmail.com>
2021-07-06 13:01:05 +0300

    Fix msToTime

--- src/helpers/DateTime.ts
+++ src/helpers/DateTime.ts
@@ -6,8 +6,27 @@ function timePad(time: number): string {
   return String(time).padStart(2, '0');
 }
 
-function onlySecs(secs: number) {
-  return `${secs}s`;
+function onlySecs(sign: string, secs: number) {
+  return `${sign}${secs}s`;
+}
+
+function timeItemsToString(
+  sign: string,
+  hrs: number,
+  mins: number,
+  secs: number,
+  showSeconds: boolean
+) {
+  if (hrs === 0 && mins === 0) {
+    return onlySecs(sign, secs);
+  }
+
+  let result = `${sign}${timePad(hrs)}:${timePad(mins)}`;
+  if (showSeconds) {
+    result += `:${timePad(secs)}`;
+  }
+
+  return result;
 }
 
 export function msToTime(s: number, showSeconds: boolean = true) {
@@ -23,16 +42,7 @@ export function msToTime(s: number, showSeconds: boolean = true) {
   const mins = s % 60;
   const hrs = (s - mins) / 60;
 
-  if (showSeconds) {
-    if (hrs === 0 && mins === 0) {
-      return onlySecs(secs);
-    }
-    return `${timePad(hrs)}:${timePad(mins)}:${timePad(secs)}`;
-  }
-  if (hrs === 0 && mins === 0) {
-    return onlySecs(secs);
-  }
-  return `${sign}${timePad(hrs)}:${timePad(mins)}`;
+  return timeItemsToString(sign, hrs, mins, secs, showSeconds);
 }
 
 export function calcDuration(taskTime: ITimeRangeModel[]): number {
