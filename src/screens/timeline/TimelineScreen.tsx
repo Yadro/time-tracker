b1e4d0e837e186a9c56e51669af7a8cc3991174b
yadro <yadrone@gmail.com>
2022-05-16 10:45:36 +0300

    Up version & small fix

--- src/screens/timeline/TimelineScreen.tsx
+++ src/screens/timeline/TimelineScreen.tsx
@@ -1,16 +1,16 @@
-import React, { FC, memo, useMemo } from 'react';
-import { Chart } from 'react-google-charts';
-import { createUseStyles } from 'react-jss';
+import React, {FC, memo, useMemo} from 'react';
+import {Chart} from 'react-google-charts';
+import {createUseStyles} from 'react-jss';
 
 import rootStore from '../../modules/RootStore';
-import { getTimeItems } from '../../helpers/TaskHelper';
+import {getTimeItems} from '../../helpers/TaskHelper';
 
-const { tasksStore } = rootStore;
+const {tasksStore} = rootStore;
 
 const columns = [
-  { type: 'string', id: 'Task' },
-  { type: 'date', id: 'Start' },
-  { type: 'date', id: 'End' },
+  {type: 'string', id: 'Task'},
+  {type: 'date', id: 'Start'},
+  {type: 'date', id: 'End'},
 ];
 
 const nowDate = new Date();
@@ -18,23 +18,20 @@ const nowDate = new Date();
 const Timeline: FC = () => {
   const classes = useStyles();
 
-  const tasks = useMemo(() => tasksStore.getTasksByDate(nowDate), []);
-  const filteredTimeItems = useMemo(() => getTimeItems(tasks, nowDate), [
-    tasks,
-  ]);
-
   const data = useMemo(() => {
-    const items = filteredTimeItems.map(({ task, time }) => [
-      task.title,
-      time.start,
+    const tasks = tasksStore.getTasksByDate(nowDate);
+    const filteredTimeItems = getTimeItems(tasks, nowDate);
+    const items = filteredTimeItems.map(({task, time}) => [
+      task.title || '-',
+      time.start || new Date(),
       time.end || new Date(),
     ]);
     return [columns, ...items];
-  }, [filteredTimeItems]);
+  }, []);
 
   return (
     <div className={classes.main}>
-      <Chart chartType="Timeline" data={data} height="100%" />;
+      <Chart style={{flex: 1}} chartType="Timeline" data={data} height='100%'/>
     </div>
   );
 };
@@ -44,5 +41,7 @@ export default memo(Timeline);
 const useStyles = createUseStyles({
   main: {
     padding: 10,
+    display: 'flex',
+    height: '100%',
   },
 });
