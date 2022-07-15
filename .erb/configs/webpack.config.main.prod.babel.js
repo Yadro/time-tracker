9ebe1e5cf9d146fb4863290683363e5f15ed5cc5
Yadro <Yadro@users.noreply.github.com>
2022-07-15 22:00:51 +0300

    Clean and fix deps (#27)
    * Fix build and clear
    
    * node 16
    
    * Fix workflow
    
    * Upd deps

--- .erb/configs/webpack.config.main.prod.babel.js
+++ .erb/configs/webpack.config.main.prod.babel.js
@@ -6,11 +6,12 @@ import path from 'path';
 import webpack from 'webpack';
 import { merge } from 'webpack-merge';
 import TerserPlugin from 'terser-webpack-plugin';
-import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import Dotenv from 'dotenv-webpack';
+// import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+
 import baseConfig from './webpack.config.base';
 import CheckNodeEnv from '../scripts/CheckNodeEnv';
 import DeleteSourceMaps from '../scripts/DeleteSourceMaps';
-import Dotenv from 'dotenv-webpack';
 
 CheckNodeEnv('production');
 DeleteSourceMaps();
@@ -46,11 +47,11 @@ export default merge(baseConfig, {
   },
 
   plugins: [
-    new BundleAnalyzerPlugin({
-      analyzerMode:
-        process.env.OPEN_ANALYZER === 'true' ? 'server' : 'disabled',
-      openAnalyzer: process.env.OPEN_ANALYZER === 'true',
-    }),
+    // new BundleAnalyzerPlugin({
+    //   analyzerMode:
+    //     process.env.OPEN_ANALYZER === 'true' ? 'server' : 'disabled',
+    //   openAnalyzer: process.env.OPEN_ANALYZER === 'true',
+    // }),
 
     /**
      * Create global constants which can be configured at compile time.
